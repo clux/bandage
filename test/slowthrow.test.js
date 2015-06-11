114aedad2f433c52c830489ada1ddfaa461a4c86
clux <analsandblaster@gmail.com>
2015-06-11 08:39:23 +0100

    an experiment

--- test/slowthrow.test.js
+++ test/slowthrow.test.js
@@ -1,9 +1,11 @@
 var test = require('../');
 
 var rejectedPromise = function () {
-  setTimeout(function () {
-    reject(new Error('hi there'));
-  }, 200);
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      reject(new Error('hi there'));
+    }, 200);
+  });
 };
 
 test('promise rejection caught', function *(t) {
