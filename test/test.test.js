1b1d6999974b1c3b854b93ca830a73c6ff9ae28a
clux <analsandblaster@gmail.com>
2015-11-25 23:51:39 +0000

    change throws to look for generic error type when looking for throws and not specifying error type

--- test/test.test.js
+++ test/test.test.js
@@ -115,17 +115,54 @@ test('Test input validation', function *T(t) {
     new Test('test without function', {});
   };
   yield t.throws(thrower, /Test function/, 'need test fn');
+  yield t.throws(thrower, TypeError, 'need test fn is TypeError');
+  yield t.throws(thrower, 'need test fn - without specific catch');
+
   var thrower2 = function *() {
     new Test('test without generator', function () {});
   }
   yield t.throws(thrower2, /is not a generator/, 'need gen fn');
+  yield t.throws(thrower2, TypeError, 'need gen fn is TypeError');
 
   var nonthrower = function *() {
     new Test('working', function *() {});
   };
   yield t.notThrows(nonthrower, 'can construct without opts');
+
   var nonthrower2 = function *() {
     new Test('working', {}, function *() {});
   };
   yield t.notThrows(nonthrower2, 'can construct with opts');
+});
+
+test('Test fail', function *T(t) {
+  var inst = new Test('invoke', function *T2(st) {
+    st.fail('this fails');
+  });
+
+  var res = yield inst.run();
+  var expected = {
+    msg: 'this fails',
+    ok: false,
+    operator: 'fail',
+    trace: 'T2 (' + __dirname + '/test.test.js:140:8)'
+  };
+
+  t.eq(res.results, [expected], 'expected results');
+  t.false(res.ok, 'tests failed');
+  t.equal(res.operator, 'test', 'run operator');
+  t.equal(res.msg, 'invoke', 'test message');
+
+  // verify tap failure message
+  var failEq = tap(expected, 1);
+  var failExp = [
+    'not ok 1 this fails',
+    '  ---',
+    '    operator: fail',
+    '    expected: undefined',
+    '    actual: undefined',
+    '    at: T2 (' + __dirname + '/test.test.js:140:8)',
+    '  ...',
+  ];
+  t.eq(failEq, failExp.join('\n'), 'tap fail message');
 });
