d8f162807ee6e100ccfc5fab9eeb8e40c5880044
clux <analsandblaster@gmail.com>
2015-11-26 21:08:27 +0000

    allow testing particular test indices via bndg using -t

--- bin.js
+++ bin.js
@@ -2,6 +2,17 @@
 
 var path = require('path');
 var test = require('.');
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv.h || argv.help) {
+  console.log('bndg [-t testIndex] testfile.js ..');
+  process.exit(0);
+}
+
+var testIndices = [];
+if (argv.t != null) {
+  var testIndices = Array.isArray(argv.t) ? argv.t : [argv.t]
+}
 
 /**
  * bndg executable
@@ -13,8 +24,8 @@ var test = require('.');
  * This will pass all js files as arguments to this file
  */
 
-process.argv.slice(2).forEach((file) => {
+argv._.forEach((file) => {
   require(path.resolve(process.cwd(), file));
 });
 
-test.run();
+test.run(testIndices);
